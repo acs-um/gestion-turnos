5626a0a6133762b5f8f7339c73ac09c1d3650f77
AlessandroGF <AlessandroGF@users.noreply.github.com>
2015-07-17 14:52:47 -0300

    Update urls.py

--- apps/turnos/urls.py
+++ apps/turnos/urls.py
@@ -5,6 +5,7 @@ from apps.turnos.views import RegistrarTurno, mostrarTurno
 
 urlpatterns = patterns('',
     url(r'^registrar/$', RegistrarTurno.as_view(),name="registrar_turno"),
-    url(r'^mostrar/$', mostrarTurno.as_view(),name="mostrar_turno")
+    url(r'^mostrar/$', mostrarTurno.as_view(),name="mostrar_turno"),
+    url(r'^borrar/(?P<pk>\d+)/$',borrarTurno.as_view(),name="eliminar_turno"),
    
 )
